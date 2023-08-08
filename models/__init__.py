dd13f5ac39cdbd8693355df8ab9c9b1823649dd2
HELLORPG <hellorpg2017@gmail.com>
2023-08-09 00:37:35 +0800

    :tada: Add code about 'build model'.

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,15 @@
-# Copyright (c) RuopengGao. All Rights Reserved.
-# About:
+# @Author       : Ruopeng Gao
+# @Date         : 2022/7/5
+import torch
+
+from utils.utils import distributed_rank
+from .memotr import build as build_memotr
+
+
+def build_model(config: dict):
+    model = build_memotr(config=config)
+    if config["AVAILABLE_GPUS"] is not None and config["DEVICE"] == "cuda":
+        model.to(device=torch.device(config["DEVICE"], distributed_rank()))
+    else:
+        model.to(device=torch.device(config["DEVICE"]))
+    return model
